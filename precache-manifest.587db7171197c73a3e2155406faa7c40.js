6740a51464faca4d74d1f396005a4db8beadfd2f
fuchsi21 <39008440+fuchsi21@users.noreply.github.com>
2020-09-11 05:56:16 +0200

    Updates

---
+++ precache-manifest.587db7171197c73a3e2155406faa7c40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec18ce0bbb38032c15de4773f6f55a65",
+    "url": "/coin-exchange/index.html"
+  },
+  {
+    "revision": "4dad0d8c08a3904e5f23",
+    "url": "/coin-exchange/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "37dcb5206547d743ba84",
+    "url": "/coin-exchange/static/js/2.75b05bf3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coin-exchange/static/js/2.75b05bf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dad0d8c08a3904e5f23",
+    "url": "/coin-exchange/static/js/main.17fd22ce.chunk.js"
+  },
+  {
+    "revision": "c9fe44687b1447b49608",
+    "url": "/coin-exchange/static/js/runtime-main.ea956e0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coin-exchange/static/media/logo.5d5d9eef.svg"
+  }
+]);
